80695a1e7602415e822ff6d7591ec2811cf62350
Duncan Kibet <kibet27@gmail.com>
2025-04-23 09:53:00 +0300

    create component

---
+++ src/Components/Destinationpage.jsx
@@ -0,0 +1,15 @@
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+function DestinationPage (){
+    const [desinations, setDestinations]= useState([]);
+
+   const [loading, setLoading] = useState(true);
+
+   const [error, setError]= useState(null);
+
+
+   useEffect(() => {
+
+    fetch()
+   })
+}
